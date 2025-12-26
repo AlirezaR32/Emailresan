b026279b846478326135288e279040226d5d27b0
AlirezaR <alireza.13rafe@gmaill.com>
2025-12-26 14:09:24 +0330

    update  main file to run all files to send mail

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("EmailResan project is running")
+from excel_reader import user_email
+from mail_sender import send_email
+from personalized_message import personalize_message
+
+for name,email in user_email.items():
+    send_email(
+        to_email= email,
+        subject="پیام تست پروژه ایمیل‌رسان",
+        message_body = personalize_message(name)
+    )
+
