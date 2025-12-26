bbd970148030c2c8fcc57a8b7c795d5c767fdfd2
AlirezaR <alireza.13rafe@gmaill.com>
2025-12-26 14:06:24 +0330

    add personal mail maker

---
+++ src/personalized_message.py
@@ -0,0 +1,13 @@
+def personalize_message(name, file_path = r"C:\Users\Alireza\Documents\uni\Emailresan\data\message.txt"):
+    # فایل رو بخون
+    with open(file_path, "r", encoding="utf-8") as f:
+        template = f.read()
+    
+    # جایگزین کن
+    message = template.replace("{name}", name)
+    return message
+
+# تست
+if __name__ == "__main__":
+    msg = personalize_message("علیرضا")
+    print(msg)
